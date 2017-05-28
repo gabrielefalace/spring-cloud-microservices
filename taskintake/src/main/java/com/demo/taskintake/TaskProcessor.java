67b57b0f6d32c75a3d5294c105b460a5041840b9
Gabriele Falace <gabriele.falace@asideas.de>
2017-05-28 20:12:25 +0200

    using new properties names

--- taskintake/src/main/java/com/demo/taskintake/TaskProcessor.java
+++ taskintake/src/main/java/com/demo/taskintake/TaskProcessor.java
@@ -2,11 +2,9 @@ package com.demo.taskintake;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.cloud.task.launcher.TaskLaunchRequest;
 import org.springframework.messaging.support.GenericMessage;
-import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
